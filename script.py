4e15f2ce120bc5d666060c233d7b071bc43d4a66
dmk28 <daniel.kik.adv@protonmail.com>
2023-04-17 16:33:22 -0300

    added prints for debugging

--- script.py
+++ script.py
@@ -14,6 +14,13 @@ from magic import Magic
 ## the 
 outlook = win32com.client.Dispatch("Outlook.Application")
 
+file_exts = {
+    'docx': ['Microsoft Word 2007+', 'Composite Document File V2 Document, Little Endian, Os: Windows, Version 6.1'],
+    'pptx': ['Microsoft PowerPoint 2007+', 'PowerPoint 97-2003'],
+    'xlsx': ['Microsoft Office 2007+', 'Composite Document File V2 Document, Little Endian, Os: Windows, Version 6.0', 'Composite Document File V2 Document, Little Endian, Os: Windows, Version 6.1'],
+    # add more file types and their corresponding magic numbers here
+}
+
 
 def check_for_outlook():
     for proc in psutil.process_iter(['pid', 'name']):
@@ -21,9 +28,9 @@ def check_for_outlook():
             if proc.name().lower() == 'outlook.exe':
                 pid = proc.pid
                 read_process(pid)
-                print("OK")
+                print("Reading process")
         except (psutil.AccessDenied, psutil.NoSuchProcess):
-            print("OK")
+            print("OK, not reading process")
             pass
 
 def read_process(pid):
@@ -39,15 +46,16 @@ def read_process(pid):
                 try:
                     with open(os.path.expandvars(f"%USERPROFILE\\Documents\\{pid}.bin"), "wb") as ext_file:
                         magic_number = Magic()
-                        extension = magic_file(magic_number, data[:1024])
-                       
-                        office_exts = ['Microsoft Word 2007+', 'Composite Document File V2 Document, Little Endian, Os: Windows, Version 6.1', 'Microsoft PowerPoint 2007+', 'PowerPoint 97-2003', 'Microsoft Office 2007+', 'Composite Document File V2 Document, Little Endian, Os: Windows, Version 6.0', 'Composite Document File V2 Document, Little Endian, Os: Windows, Version 6.1']
-                        if extension in office_exts:
-                            file_extension = os.path.splitext(extension)[1]
-                            output_file_name = f"{pid}{file_extension}"
-                            with open(output_file_name, "wb") as export_file:
-                                export_file.write(data)
-                            print(f"Extraction successful: {output_file_name}")
+                        file_type = magic_number.from_buffer(data[:1024])
+                        for ext, magic_nums in file_exts.items():
+                            if file_type in magic_nums:
+                                output_file_name = f"{pid}.{ext}"
+                                with open(output_file_name, "wb") as export_file:
+                                    export_file.write(data)
+                                print(f"Extraction successful: {output_file_name}")
+                                break
+
+
                 except Exception as e:
                     print("Couldn't read byte-array:", e)
         except:
